66ee83fe84e12c262d7c936876924c6f6fe9e6a7
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-12-13 15:42:12 +0330

    fx

--- src/views/pages/HomePage.js
+++ src/views/pages/HomePage.js
@@ -3,16 +3,23 @@ import Material from "../../seyed-modules/components/Material"
 import ArrowSvg from "../../media/svg/ArrowSvg"
 import pic from "../../media/images/main.png"
 import ImageShow from "../../seyed-modules/components/ImageShow"
-import GetBanks from "../../hooks/GetBanks"
 import getImage from "../../helpers/getImage"
 import Footer from "../containers/Footer"
 import GetBanners from "../../hooks/GetBanners"
 import Link from "../../seyed-modules/components/Link"
 import urlConstant from "../../constant/urlConstant"
+import Bank1Svg from "../../media/svg/Bank1Svg"
+import Bank2Svg from "../../media/svg/Bank2Svg"
+import Bank3Svg from "../../media/svg/Bank3Svg"
+import Bank4Svg from "../../media/svg/Bank4Svg"
+import Bank5Svg from "../../media/svg/Bank5Svg"
+import Bank6Svg from "../../media/svg/Bank6Svg"
+import Bank7Svg from "../../media/svg/Bank7Svg"
+import Bank8Svg from "../../media/svg/Bank8Svg"
+import Bank9Svg from "../../media/svg/Bank9Svg"
 
 function HomePage()
 {
-    const {data} = GetBanks()
     const {data: banners} = GetBanners()
     return (
         <>
@@ -32,11 +39,15 @@ function HomePage()
                 </div>
             </div>
             <div className="ranking-page-banks">
-                {
-                    data.map(item =>
-                        <ImageShow key={item._id} src={getImage(item.logo)} className="ranking-page-banks-item"/>,
-                    )
-                }
+                <Bank1Svg className="ranking-page-banks-item"/>
+                <Bank2Svg className="ranking-page-banks-item"/>
+                <Bank3Svg className="ranking-page-banks-item"/>
+                <Bank4Svg className="ranking-page-banks-item"/>
+                <Bank5Svg className="ranking-page-banks-item"/>
+                <Bank6Svg className="ranking-page-banks-item"/>
+                <Bank7Svg className="ranking-page-banks-item"/>
+                <Bank8Svg className="ranking-page-banks-item"/>
+                <Bank9Svg className="ranking-page-banks-item"/>
             </div>
             <div className="ranking-page-banners">
                 {
